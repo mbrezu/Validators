e3bec52623b6c4fae9ecc02873ffb4b7cb85014d
Miron Brezuleanu <mbrezu@gmail.com>
2023-05-22 12:22:59 +0300

    Tests for JSON objects.

---
+++ Validators.Test/JsonObject.cs
@@ -0,0 +1,166 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using static Validators.NewtonsoftJson.Json;
+
+namespace Validators.Test
+{
+    public class JsonObject
+    {
+        [Fact]
+        public void ValidKeysPasses()
+        {
+            // Arrange
+            var target = JObject.Parse("""
+                {
+                    "a": true
+                }
+                """);
+            var validator = ValidKeys("a");
+
+            // Act
+            var errors = validator.Validate(target);
+
+            // Assert
+            errors.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ValidKeysFails()
+        {
+            // Arrange
+            var target = JObject.Parse("""
+                {
+                    "a": true,
+                    "b": null
+                }
+                """);
+            var validator = ValidKeys("a");
+
+            // Act
+            var errors = validator.Validate(target);
+
+            // Assert
+            errors.Should().HaveCount(1);
+            errors.First().Message.Should().Be("Key 'b' is not valid.");
+            errors.First().Path.Should().BeEmpty();
+        }
+  
+        [Fact]
+        public void HasKeyPasses()
+        {
+            // Arrange
+            var target = JObject.Parse("""
+                {
+                    "a": true
+                }
+                """);
+            var validator = HasKey("a");
+
+            // Act
+            var errors = validator.Validate(target);
+
+            // Assert
+            errors.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void HasKeyFails()
+        {
+            // Arrange
+            var target = JObject.Parse("""
+                {
+                    "b": true
+                }
+                """);
+            var validator = HasKey("a");
+
+            // Act
+            var errors = validator.Validate(target);
+
+            // Assert
+            errors.Should().HaveCount(1);
+            errors.First().Message.Should().Be("Doesn't have key 'a'.");
+            errors.First().Path.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void RequiredKeysPasses()
+        {
+            // Arrange
+            var target = JObject.Parse("""
+                {
+                    "a": true,
+                    "b": false
+                }
+                """);
+            var validator = RequiredKeys("a", "b");
+
+            // Act
+            var errors = validator.Validate(target);
+
+            // Assert
+            errors.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void RequiredKeysFails()
+        {
+            // Arrange
+            var target = JObject.Parse("""
+                {
+                    "a": true,
+                    "b": false
+                }
+                """);
+            var validator = RequiredKeys("a", "b", "c");
+
+            // Act
+            var errors = validator.Validate(target);
+
+            // Assert
+            errors.Should().HaveCount(1);
+            errors.First().Message.Should().Be("Key 'c' is missing.");
+            errors.First().Path.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void DiveIntoPasses()
+        {
+            // Arrange
+            var target = JObject.Parse("""
+                {
+                    "a": true,
+                    "b": false
+                }
+                """);
+            var validator = DiveInto("a", IsBoolean);
+
+            // Act
+            var errors = validator.Validate(target);
+
+            // Assert
+            errors.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void DiveIntoFails()
+        {
+            // Arrange
+            var target = JObject.Parse("""
+                {
+                    "a": true,
+                    "b": false
+                }
+                """);
+            var validator = DiveInto("a", IsNull);
+
+            // Act
+            var errors = validator.Validate(target);
+
+            // Assert
+            errors.Should().HaveCount(1);
+            errors.First().Message.Should().Be("Not 'null'.");
+            errors.First().Path.Should().BeEquivalentTo(new string[] { "a" });
+        }
+    }
+}
