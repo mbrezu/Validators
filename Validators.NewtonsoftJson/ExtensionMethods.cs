0c1a32a4a8cca855a541a6aeee6352a571764103
Miron Brezuleanu <mbrezu@gmail.com>
2023-05-22 13:41:02 +0300

    Faulty JSON content extraction.

---
+++ Validators.NewtonsoftJson/ExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using Validators.Core;
+
+namespace Validators.NewtonsoftJson
+{
+    public static class ExtensionMethods
+    {
+        public static JToken? ExtractInvalidContent(this IValidationError error, JToken? content)
+        {
+            static JToken? extract(JToken? content, IEnumerable<string> path)
+            {
+                if (!path.Any())
+                {
+                    return content;
+                }
+                if (content is JObject obj)
+                {
+                    var key = path.First();
+                    return extract(content[key], path.Skip(1));
+                }
+                else if (content is JArray arr && int.TryParse(path.First(), out var index))
+                {
+                    return extract(content[index], path.Skip(1));
+                }
+                else
+                {
+                    return content;
+                }
+            }
+            return extract(content, error.Path);
+        }
+    }
+}
---
+++ Validators.Test/ContentExtraction.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using Validators.NewtonsoftJson;
+using static Validators.NewtonsoftJson.Json;
+
+namespace Validators.Test
+{
+    public class ContentExtraction
+    {
+        [Fact]
+        public void ExtractContent()
+        {
+            // Arrange
+            var target = JObject.Parse("""
+                {
+                    "a": 1,
+                    "b": true,
+                    "c": [1, 2, 3, "fail"]
+                }
+                """);
+            var validator = And(
+                IsObject,
+                HasRequiredKeys("a", "b", "c"),
+                HasValidKeys("a", "b", "c"),
+                DiveInto("c", IsArrayOf(IsNumber)));
+
+            // Act
+            var errors = validator.Validate(target);
+            var content = errors.ElementAt(0).ExtractInvalidContent(target);
+
+            // Assert
+            content.Should().NotBeNull();
+            content!.Type.Should().Be(JTokenType.String);
+            content!.Value<string>()!.Should().Be("fail");
+        }
+    }
+}
